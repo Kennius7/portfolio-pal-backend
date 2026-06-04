78d93545df543dc5bfe7e2992fbe9da9ec8cdb0d
Kennius7 <ogbogukenny@yahoo.com>
2026-06-04 15:51:38 +0100

    feat: add email service fix auth controller - 03

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -2,6 +2,13 @@ import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { MailService } from './mail.service';
 
+console.log({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  user: process.env.MAIL_USER,
+  password: process.env.MAIL_PASSWORD,
+});
+
 @Module({
   imports: [
     MailerModule.forRoot({
