735c0534449f6449c9405aabeb712ce4d00585d1
Kennius7 <ogbogukenny@yahoo.com>
2026-06-04 15:58:51 +0100

    feat: add email service fix auth controller - 04

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { MailService } from './mail.service';
+import * as nodemailer from 'nodemailer';
 
 console.log({
   host: process.env.MAIL_HOST,
@@ -8,6 +9,25 @@ console.log({
   user: process.env.MAIL_USER,
   password: process.env.MAIL_PASSWORD,
 });
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: Number(process.env.MAIL_PORT),
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD,
+  },
+});
+
+transporter
+  .verify()
+  .then(() => {
+    console.log('SMTP server reachable');
+  })
+  .catch((error: unknown) => {
+    console.log('SMTP server not reachable', error);
+  });
 
 @Module({
   imports: [
